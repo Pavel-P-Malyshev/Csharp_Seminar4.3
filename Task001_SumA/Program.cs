4fe21779b5c43860d4af85091d4f636faa7b9a12
Pavel <m83819@gmail.com>
2022-08-17 11:18:50 +0700

    Task001_SumA

---
+++ Task001_SumA/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает 
+на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+int SumA(int A)
+{
+    int answer=0;
+    for(int i=1;i<=A;i++) {answer+=i;}
+    return answer;
+
+}
+
+double GaussSumA(int A)
+{
+    return (A+1)*(A/2.0);
+}
+
+int a=Prompt("eneter A: ");
+while(a<1) {a=Prompt("A cannot be less than 1, enter correct A: ");}
+System.Console.WriteLine($"sum of natuaral numbers between 1 and A is: {SumA(a)}");
+System.Console.WriteLine($"Gauss sum of natuaral numbers between 1 and A is: {GaussSumA(a)}");
